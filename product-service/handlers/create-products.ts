7b9b1b31853767f35744e6987eef27add606f209
lithium <lithiumfallen@yandex.by>
2020-11-10 22:59:05 +0300

    fix: add yup for shema validation

--- product-service/handlers/create-products.ts
+++ product-service/handlers/create-products.ts
@@ -4,38 +4,23 @@ import { Client } from 'pg';
 import headers from './headers';
 import generateDbConfig from '../db/db-config';
 import { addNewProduct } from '../db/queries';
-
-type BodyTypes = {
-  title: string,
-  description: string,
-  count: number,
-  price: number,
-}
-
-const isBodyInvalid = (body: BodyTypes) : boolean => {
-  if (body === null || body === undefined) return true;
-  const { title, description, count, price } = body;
-  return (typeof title !== 'string'
-    && typeof description !== 'string'
-    && typeof count !== 'number'
-    && typeof price !== 'number'
-  );
-}
+import { Product, ProductSchema } from '../db/models/Product';
 
 const createProducts: APIGatewayProxyHandler = async (event, _context) => {
   console.log('Invoke createProducts lambda\n');
   console.log('ENVIRONMENT VARIABLES:' + JSON.stringify(process.env, null, 2));
   console.info('EVENT:' + JSON.stringify(event, null, 2));
-  
-  const body: BodyTypes = JSON.parse(event.body);
+ 
+  const body: Product = JSON.parse(event.body);
+  const isValid = await ProductSchema.isValid(body);
 
-  if (isBodyInvalid(body)) {
+  if (!isValid) {
     return {
       statusCode: 400,
       headers,
       body: 'Product data is invalid'
     }
-  } 
+  }
 
   const dbClient = new Client(generateDbConfig(process));
 
