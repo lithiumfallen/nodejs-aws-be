cf07cb2678cb2815983dc110f2d223ea63171f6b
lithium <lithiumfallen@yandex.by>
2020-11-15 17:01:41 +0300

    feat: add importProductsFile logic

--- import-service/handlers/import-products-file.ts
+++ import-service/handlers/import-products-file.ts
@@ -1,14 +1,25 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 import 'source-map-support/register';
+import generateResponseObject from './response';
 
 export const importProductsFile: APIGatewayProxyHandler = async (event, _context) => {
   console.log('importProductFile start', event.queryStringParameters.name)
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+  try {
+    const { name } = event.queryStringParameters;
+    const path = `uploaded/${name}`;
+    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    const s3Params = {
+      Bucket: 'node-aws-s3-import',
+      Key: path,
+      Expires: 60,
+      ContentType: 'text/csv',
+    }
+    const url = await s3.getSignedUrlPromise('putObject', s3Params);
+
+    return generateResponseObject(200, url)
+  } catch (error) {
+    return generateResponseObject(500, error)
+  }
 }
